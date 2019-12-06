10f76130a7de3243a7d9fa9638a99a1a1ba437df
kee cha <kee_cha18@yahoo.com>
2019-12-06 14:18:30 -0600

    finish adding five test method for add method

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        private int count;
+        private int capacity;
+        private T[] items;
+        
+
+        public CustomList()
+        {
+            count = 0;
+            capacity = 4;
+            items = new T[capacity];
+        }
+        public T this[int index] 
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+            }
+                 
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+        public void Add()
+        {
+            CustomList<T> items = new CustomList<T>();
+            
+            count++;
+        }
+    }
+}
--- CustomListTest/UnitTest1.cs
+++ CustomListTest/UnitTest1.cs
@@ -5,11 +5,86 @@ using CustomList;
 namespace CustomListTest
 {
     [TestClass]
-    public class UnitTest1
+    public class CustomListTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_OnePositiveNumberToList()
         {
+            //Arrange
+            CustomList <int> items = new CustomList<int>();
+            double expectedResult = 2;
+            double actual;
+
+            //Act
+            items.Add(2);
+            actual = items[0];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actual); //Checking to see if added value is in list
+        }
+        [TestMethod]
+        public void Add_MultiplePositiveNumbersToList()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            double expectedResult = 4;
+
+            //Act
+            items.Add(2);
+            items.Add(4);
+            items.Add(6);
+            double actual = items[1];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actual); //checking to see if certain value are in list when multiple values are added
+        }
+
+        [TestMethod]
+        public void Add_CheckingAmountOfItemsInList()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            double expectedResult = 3;
+            double actual;
+
+            //Act
+            items.Add(2);
+            items.Add(4);
+            items.Add(6);
+            actual = items.Count;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actual); //checking to see if the amount of items in list is counted
+        }
+
+        [TestMethod]
+        public void Add_StringsToList()
+        {
+            //Arrange
+            CustomList<string> items = new CustomList<string>();
+            string expectedResult = "Kee";
+            //Act
+            items.Add("Kee");
+            string actualResult = items[0];
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult); //Checking to see if different data type added are the same in index position
+        }
+
+        [TestMethod]
+        public void Add_CheckingToSeeIfCountInListGrow()
+        {
+            //Arrange
+            CustomList<string> items = new CustomList<string>();
+            int expected = 5;
+            //Act
+            items.Add("cha");
+            items.Add("cha");
+            items.Add("cha");
+            items.Add("cha");
+            items.Add("cha");
+            int actual = items.Count;
+            //Assert
+            Assert.AreEqual(expected, actual); //checking to see if Count grow past the initial array
         }
     }
 }
